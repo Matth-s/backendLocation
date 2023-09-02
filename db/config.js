4542a95357350370227eb0e1e462e664726ddd02
Matth-s <senosiain.matthieu@yahoo.fr>
2023-09-02 19:20:39 +0200

    apikey test

--- db/config.js
+++ db/config.js
@@ -7,7 +7,7 @@ const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Remplacez les caractères '\\n' par de vrais sauts de ligne
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -19,8 +19,7 @@ const serviceAccount = {
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL:
-    "https://test-62e62-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: process.env.FIREBASE_DATA_BASE_URL_HTTP,
 });
 
 module.exports = admin;
