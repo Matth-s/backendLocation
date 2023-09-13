17836fd239adc60243a8f1f9f435caba11e80968
Matth-s <108790362+Matth-s@users.noreply.github.com>
2023-09-13 18:18:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "http://localhost:3001"],
+      scriptSrc: ["'self'", "http://localhost:3000"],
     },
   })
 );
